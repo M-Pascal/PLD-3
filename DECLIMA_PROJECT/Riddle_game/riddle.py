2214e5eedf31424263b486cc27f93d0bcd397f38
Enzo Bat <e.batungwan@alustudent.com>
2023-08-04 21:02:23 +0200

    riddle-code

--- DECLIMA_PROJECT/Riddle_game/riddle.py
+++ DECLIMA_PROJECT/Riddle_game/riddle.py
@@ -3,8 +3,8 @@ import random
 
 def main():
     display_instructions()
-    play_riddle_game()
-
+    riddles = select_riddle()
+    play_riddle_game(riddles)
 
 
 #Displays the instructions
@@ -18,7 +18,7 @@ def display_instructions():
 
 
 #Select a random riddle in the list
-def select_random_riddle():
+def select_riddle():
     riddles = [
         {"question" : "I am essential for life, but excessive amounts are causing global warming. What am I?",
          "answer" : "carbon dioxide"},
@@ -27,18 +27,36 @@ def select_random_riddle():
         {"question" : "I am a gas that's helpful to plants, but I cannot be bought at a gas station. What am I?",
          "answer" : "carbon dioxide"}
     ]
-    return random.choice(riddles)
+    return riddles  
 
 
 #start playing
 def play_riddle_game(riddles):
     score = 0 
-   
-    for i in len(riddles):
-        riddle = select_random_riddle()
+
+#The for loop so it ends when all riddles have been used
+    for riddle in riddles:
         lives_left = 3
 
+        print("\nRiddle: " + riddle["question"])
+        while lives_left > 0:
+            user_guess = input("Your Answer ({} lives left): ".format(lives_left)).lower()
+# Verify user guess, and remove one life if the user fail.            
+            if user_guess == riddle["answer"]:
+                print("Congratulations! That's the correct answer!")
+                score += 1
+                break
+            else:
+                lives_left -= 1
+                if lives_left > 0:
+                    print("Incorrect answer. Try again!")
+                else:
+                    print("GAME OVER! You've used all your lives. The correct answer was: '{}'.".format(riddle["answer"]))
 
+    print("\nThank you for playing! Your final score: {} out of {} riddles.".format(score, len(riddles)))
+
+        
+        
 
 
 if __name__ == '__main__':
